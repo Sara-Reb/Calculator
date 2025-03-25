662eedf4d27d834f965f154a02987170ed72b3ab
Sara <sara.rebecca360@gmail.com>
2025-03-25 16:33:40 +0100

    Add operation function

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(x, y) {
 function divide(x, y) {
   return x, y;
 }
+
+const num1 = null;
+const num2 = null;
+const op = null;
+
+function operation(num1, num2, op) {
+  if (op === "+") {
+    return add(num1, num2);
+  } else if (op === "-") {
+    return subtract(num1, num2);
+  } else if (op === "x") {
+    return multiply(num1, num2);
+  } else if (op === "÷") {
+    return divide(num1, num2);
+  }
+}
